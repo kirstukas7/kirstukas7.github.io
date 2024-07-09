ffad3d63144143e07f87e87275de5aae4fd4c146
kirstukas7 <49204315+kirstukas7@users.noreply.github.com>
2024-07-09 21:51:00 +0300

    Minimalus funkcionalumas

---
+++ scripts/vrkr.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([54.8750, 23.9093], 12);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
