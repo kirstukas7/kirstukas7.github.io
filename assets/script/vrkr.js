167d5e40d3d86e7f48a9eaadc46ab716afb3d508
kirstukas7 <49204315+kirstukas7@users.noreply.github.com>
2024-07-10 22:56:21 +0300

    Stilių palaikymas

--- assets/script/vrkr.js
+++ assets/script/vrkr.js
@@ -1,3 +1,5 @@
+
+
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     minZoom: 10,
     maxZoom: 15,
@@ -6,9 +8,23 @@ var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 map.setView([54.8750, 23.9093], 10);
 
-var layerKeliai = new L.GeoJSON.AJAX("../assets/geojson/vrkr_keliai.geojson").addTo(map);
-layerKeliai.useSimpleStyle();
-var layerSkiriamosios = L.GeoJSON.AJAX("../assets/geojson/vrkr_skiriamosios.geojson").addTo(map);
-layerSkiriamosios.useSimpleStyle();
-var layerSankryzos = L.GeoJSON.AJAX("../assets/geojson/vrkr_sankryzos.geojson").addTo(map);
-layerSankryzos.useSimpleStyle();
+$.getJSON("/assets/geojson/vrkr_keliai.geojson", function (vrkr_keliai) {
+    displayJSON(vrkr_keliai);
+});
+
+$.getJSON("/assets/geojson/vrkr_skiriamosios.geojson", function (vrkr_skiriamosios) {
+    displayJSON(vrkr_skiriamosios);
+});
+
+$.getJSON("/assets/geojson/vrkr_sankryzos.geojson", function (vrkr_sankryzos) {
+    displayJSON(vrkr_sankryzos);
+});
+
+function displayJSON(jsonObject) {
+    L.geoJSON(jsonObject, {
+        useSimpleStyle: true,
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.title);
+        }
+    }).addTo(map);
+};
