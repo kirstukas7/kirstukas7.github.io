494c92bf36158174464d18721665d2f99650bc0b
kirstukas7 <49204315+kirstukas7@users.noreply.github.com>
2024-07-10 21:09:23 +0300

    Duomenų pridėjimas

--- assets/script/vrkr.js
+++ assets/script/vrkr.js
@@ -1,6 +1,14 @@
-var map = L.map('map').setView([54.8750, 23.9093], 12);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    minZoom: 10,
+    maxZoom: 15,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+}).addTo(map);
+
+map.setView([54.8750, 23.9093], 10);
+
+var layerKeliai = new L.GeoJSON.AJAX("../assets/geojson/vrkr_keliai.geojson").addTo(map);
+layerKeliai.useSimpleStyle();
+var layerSkiriamosios = L.GeoJSON.AJAX("../assets/geojson/vrkr_skiriamosios.geojson").addTo(map);
+layerSkiriamosios.useSimpleStyle();
+var layerSankryzos = L.GeoJSON.AJAX("../assets/geojson/vrkr_sankryzos.geojson").addTo(map);
+layerSankryzos.useSimpleStyle();
